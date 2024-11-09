c7deba5c5a16e01a325ef5904b185751cffc025b
mazefei <mail.yunzhou-tech.com>
2024-11-09 15:09:12 +0800

    cmake 编译

---
+++ build/CMakeFiles/a.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../bin/a"
+  "../bin/a.pdb"
+  "CMakeFiles/a.dir/src/AISPare.cpp.o"
+  "CMakeFiles/a.dir/src/Time.cpp.o"
+  "CMakeFiles/a.dir/src/file.cpp.o"
+  "CMakeFiles/a.dir/src/main.cpp.o"
+  "CMakeFiles/a.dir/src/type_haddle.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
